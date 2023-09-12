e8d620e7b8144941b0a89b1381cd3993b5238e63
AntonKo24 <anton.korieniev.dev@gmail.com>
2023-09-12 12:43:26 +0300

    small change

--- app/src/main/java/com/tonyk/android/homeworkfragments/model/Contact.kt
+++ app/src/main/java/com/tonyk/android/homeworkfragments/model/Contact.kt
@@ -1,13 +1,13 @@
 package com.tonyk.android.homeworkfragments.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 import java.util.UUID
 
 @Parcelize
 data class Contact (
     val id: UUID,
-    var name: String,
-    var surname : String,
-    var phoneNumber : String
+    val name: String,
+    val surname : String,
+    val phoneNumber : String
 ) : Parcelable
--- app/src/main/java/com/tonyk/android/homeworkfragments/ui/ContactsListAdapter.kt
+++ app/src/main/java/com/tonyk/android/homeworkfragments/ui/ContactsListAdapter.kt
@@ -45,7 +45,6 @@ class ContactDiffCallback : DiffUtil.ItemCallback<Contact>() {
     override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
         return oldItem.id == newItem.id
     }
-
     override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
         return oldItem == newItem
     }
